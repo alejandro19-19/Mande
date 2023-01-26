312e4b5dfdb49cf0f9bc1f315860c61b4a3423bc
alejandro19-19 <65925378+alejandro19-19@users.noreply.github.com>
2023-01-26 18:47:10 -0500

    Actualizacion base de datos
    cambios en los schemas

--- database/createdb.sql
+++ database/createdb.sql
@@ -40,13 +40,14 @@ CREATE TABLE servicio (
 );
 
 CREATE TABLE tarjeta (
-    id SERIAL PRIMARY KEY,
-    numero INT PRIMARY KEY,
+    id SERIAL,
+    numero INT,
     id_cliente INT NOT NULL,
     tipo VARCHAR(100) NOT NULL,
     cvv VARCHAR(100) NOT NULL,
     nombre_titular VARCHAR(100) NOT NULL,
     fecha_vencimiento DATE NOT NULL,
+    PRIMARY KEY(id,numero),
     CONSTRAINT fk_cliente_tarjeta
         FOREIGN KEY (id_cliente) REFERENCES cliente(id)
 );
@@ -54,24 +55,26 @@ CREATE TABLE tarjeta (
 CREATE TABLE pago (
     id SERIAL PRIMARY KEY,
     id_cliente INT NOT NULL,
+    numero_tarjeta INT NOT NULL,
     id_tarjeta INT NOT NULL,
     id_trabajador INT NOT NULL,
     valor_pago INT NOT NULL,
     CONSTRAINT fk_cliente_pago
         FOREIGN KEY (id_cliente) REFERENCES cliente(id) ON UPDATE CASCADE ON DELETE SET NULL,
     CONSTRAINT fk_tarjeta_pago
-        FOREIGN KEY (id_tarjeta) REFERENCES tarjeta(id) ON UPDATE CASCADE ON DELETE SET NULL,
+        FOREIGN KEY (id_tarjeta, numero_tarjeta) REFERENCES tarjeta(id,numero) ON UPDATE CASCADE ON DELETE SET NULL,
     CONSTRAINT fk_trabajador_pago
         FOREIGN KEY (id_trabajador) REFERENCES trabajador(id) ON UPDATE CASCADE ON DELETE SET NULL
 );
 
 CREATE TABLE contratacion(
-    id SERIAL PRIMARY KEY,
-    id_cliente INT PRIMARY KEY,
-    id_trabajador INT PRIMARY KEY,
-    id_servicio INT PRIMARY KEY,
+    id SERIAL,
+    id_cliente INT,
+    id_trabajador INT,
+    id_servicio INT,
     descripcion_trabajo VARCHAR(300),
     calificacion_servicio FLOAT NOT NULL,
+    PRIMARY KEY (id, id_cliente, id_trabajador,id_servicio),
     CONSTRAINT fk_cliente_contratacion
         FOREIGN KEY (id_cliente) REFERENCES cliente(id) ON UPDATE CASCADE ON DELETE CASCADE,
     CONSTRAINT fk_trabajador_contratacion
@@ -80,11 +83,13 @@ CREATE TABLE contratacion(
         FOREIGN KEY (id_servicio) REFERENCES servicio(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
+
 CREATE TABLE prestar_servicio (
-    id_trabajador INT PRIMARY KEY,
-    id_servicio INT PRIMARY KEY,
+    id_trabajador INT,
+    id_servicio INT,
     calificacion FLOAT NOT NULL,
     valor_fraccion INT NOT NULL,
+    PRIMARY KEY (id_trabajador, id_servicio),
     CONSTRAINT fk_trabajadro_prestar_servicio
         FOREIGN KEY (id_trabajador) REFERENCES trabajador(id) ON UPDATE CASCADE ON DELETE CASCADE,
     CONSTRAINT fk_servicio_prestar_servicio
