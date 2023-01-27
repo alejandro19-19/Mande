ea6a31504f49bcf60d53915dcd401d6622ace0fc
alejandro19-19 <65925378+alejandro19-19@users.noreply.github.com>
2023-01-27 15:25:59 -0500

    inserts basicos
    inserts basicos para pruebas

--- database/queries.sql
+++ database/queries.sql
@@ -9,4 +9,16 @@
 CREATE VIEW trabajadores_disponibles AS 
  SELECT * FROM trabajador WHERE disponible = true;
 
- /* Consultas para procedimientos */
+ /* Consultas para procedimientos */ 
+ 
+ //consultas de insercion. Recuerde cambiar la tabla de trabajador
+ 
+ INSERT INTO  trabajador(nombre,apellidos,email,numero_celular,fecha_nacimiento,direccion_residencia,documento_identidad,foto_perfil) VALUES 
+('alejandro', 'escobar', 'alejandro@gmail.com', '1234', '2019-01-01', 'calle 5 prueba', 'direcciondeldocumento', 'foto'),
+('alejandro', 'peñaranda', 'peñaranda@gmail.com', '1234', '2019-01-01', 'calle 4 prueba', 'direcciondeldocumento', 'foto')
+;
+
+INSERT INTO  cliente(nombre,apellidos,email,numero_celular,fecha_nacimiento,direccion_residencia,recibo_servicio_publico) VALUES 
+('juan', 'santa', 'juan@gmail.com', '1234', '2019-01-01', 'calle 5 prueba', 'recibo'),
+('miguel', 'fernandez', 'miguel@gmail.com', '1234', '2019-01-01', 'calle 4 prueba', 'elrecibo')
+;
