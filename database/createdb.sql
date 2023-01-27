eb62abc17eaa579f94554b87305750cc532c95ed
alejandro19-19 <65925378+alejandro19-19@users.noreply.github.com>
2023-01-27 18:15:53 -0500

    Update createdb.sql

--- database/createdb.sql
+++ database/createdb.sql
@@ -41,7 +41,7 @@ CREATE TABLE servicio (
 
 CREATE TABLE tarjeta (
     id SERIAL,
-    numero VARCHAR(100),
+    numero VARCHAR(150),
     id_cliente INT NOT NULL,
     tipo VARCHAR(100) NOT NULL,
     cvv VARCHAR(100) NOT NULL,
@@ -55,7 +55,7 @@ CREATE TABLE tarjeta (
 CREATE TABLE pago (
     id SERIAL PRIMARY KEY,
     id_cliente INT NOT NULL,
-    numero_tarjeta VARCHAR(100) NOT NULL,
+    numero_tarjeta VARCHAR(150) NOT NULL,
     id_tarjeta INT NOT NULL,
     id_trabajador INT NOT NULL,
     valor_pago INT NOT NULL,
