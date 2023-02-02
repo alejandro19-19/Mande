ae4688b518a18c48770e265371597c1b9787ede7
alejandro19-19 <65925378+alejandro19-19@users.noreply.github.com>
2023-02-02 12:00:06 -0500

    Actualizacion login
    Informacion necesaria del login

--- routes/login.js
+++ routes/login.js
@@ -13,24 +13,24 @@ router.post('/', function (req, res, next) {
 
     if (req.body.tipo == "cliente") {
       const verificarCliente = async () => {
-        cliente = await client.query(`SELECT email, numero_celular FROM cliente WHERE email = '${req.body.email}' and numero_celular = '${req.body.numero_celular}';`)
+        let cliente = await client.query(`SELECT * FROM cliente WHERE email = '${req.body.email}' and numero_celular = '${req.body.numero_celular}';`)
         if (cliente.rows.length == 0) {
           res.status(401).json({ error: true, informacion: "el usuario no existe" });
         }
         else {
-          res.status(200).json({ error: false, informacion: "se ha logueado exitosamente" });
+          res.status(200).json({ error: false, informacion: "se ha logueado exitosamente", usuario: cliente.rows[0]});
         }
       }
       verificarCliente().then();
     }
     else if (req.body.tipo == "trabajador") {
       const verificarTrabajador = async () => {
-        cliente = await client.query(`SELECT email, numero_celular FROM trabajador WHERE email = '${req.body.email}' and numero_celular = '${req.body.numero_celular}';`)
+        let cliente = await client.query(`SELECT * FROM trabajador WHERE email = '${req.body.email}' and numero_celular = '${req.body.numero_celular}';`)
         if (cliente.rows.length == 0) {
           res.status(401).json({ error: true, informacion: "el usuario no existe" });
         }
         else {
-          res.status(200).json({ error: false, informacion: "se ha logueado exitosamente" });
+          res.status(200).json({ error: false, informacion: "se ha logueado exitosamente", usuario: cliente.rows[0]});
         }
       }
       verificarTrabajador().then();
